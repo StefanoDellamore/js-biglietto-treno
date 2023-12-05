c42bc4e831adbb52467db795e0ff8f06ae0e519b
StefanDellamore <dellamoreste.info@gmail.com>
2023-12-05 21:40:07 +0100

    Verifica e applicazione sconto per minorenni e over-65

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,36 @@ const price = (kmInNumber * 0.21);
 console.log ('price', price, typeof price);
 
 let finalText = 'il prezzo del biglietto è $:';
+
+//verifivo sconto da applicare
+
+if (ageInNumber < 18) {
+    
+    //applico 20% di sconto
+    const discount = (price / 100) * 20;
+    console.log ('discount', discount, typeof discount);
+    
+    //prezzo scontato
+    const discountedPrice = price - discount;
+    console.log ('discountedPrice', discountedPrice, typeof discountedPrice);
+    finalText = finalText + discountedPrice.toFixed(2);
+}
+
+else if (ageInNumber > 65) {
+    //applico 40% di sconto
+    const discount = (price / 100) * 40;
+    console.log ('discount', discount, typeof discount);
+    
+    //prezzo scontato
+    const discountedPrice = price - discount;
+    console.log ('discountedPrice', discountedPrice, typeof discountedPrice);
+    finalText = finalText + discountedPrice.toFixed(2); 
+}
+
+else {
+    finalText = finalText + price.toFixed(2);
+}
+
+const resultContainer = document.querySelector ('#result-container');
+console.log ('resultContainer', resultContainer, typeof resultContainer);
+resultContainer.innerHTML = finalText;
