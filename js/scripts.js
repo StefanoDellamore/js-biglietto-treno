dae284faa586a5e9f429fe2e816f256f787d1f2c
StefanDellamore <dellamoreste.info@gmail.com>
2023-12-05 21:38:15 +0100

    Richiesta età utente

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,11 @@ const kmInNumber = parseInt (km);
 console.log ('km da percorrere:', km, typeof km);
 console.log ('kmInNumber', kmInNumber, typeof km)
 if (isNaN(km)) {alert('Scrivi i km in numeri!');}
+
+//chiedo età all'utente
+
+const age = prompt ('Inserisci anni')
+const ageInNumber = parseInt (age);
+
+console.log ('anni:', age, typeof age);
+console.log ('ageInNumber', ageInNumber, typeof age);
